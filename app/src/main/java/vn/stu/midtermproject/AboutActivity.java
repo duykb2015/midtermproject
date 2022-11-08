c3c9b71de23555c44afe65261b08173a6279038c
Bế Lãng Duy <duyx4541@gmail.com>
2022-11-08 12:11:29 +0700

    #add about fearture

--- app/src/main/java/vn/stu/midtermproject/AboutActivity.java
+++ app/src/main/java/vn/stu/midtermproject/AboutActivity.java
@@ -10,5 +10,6 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
     }
 }
